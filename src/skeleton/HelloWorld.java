e4127624abfdf7f5a231989d13b886690608d868
INB201Squared <joannesis@gmail.com>
2011-03-15 17:06:35 +1000

    Test change

--- src/skeleton/HelloWorld.java
+++ src/skeleton/HelloWorld.java
@@ -1,9 +1,11 @@
 package skeleton;
 
 public class HelloWorld {
-	
-	
+		
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		System.out.println("WhynoWork?");
+		
+		System.out.println("Another test");
 	}
 }
